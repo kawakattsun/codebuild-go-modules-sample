b43415d82862329dee26b5b82dc070902c2cf3fc
kawakattsun <riverwin.akira@gmail.com>
2020-11-06 18:40:14 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kawakattsun/codebuild-go-modules-sample
+
+go 1.15
+
+require github.com/kawakattsun/codebuild-go-modules-sample-private v0.1.0 // indirect
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/kawakattsun/codebuild-go-modules-sample-private/version"
+
+func main() {
+	version.ShowVersion("codebuild-go-modules-sample")
+}
